36907695a2a1ab6c2f087621bdc5d9bc0dbdcce3
Aabhasmishra <104935350+Aabhasmishra@users.noreply.github.com>
2025-05-15 15:04:40 +0530

    Add files via upload

---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+// import styles from "./HomePage.module.css"
+import { Button } from "../../Styled/Button";
+
+const HomePage = ( { toggle } ) => {
+  return (
+    <Container>
+      <img src="/public/Images/dices-1.png" />
+      <div className="content">
+        <h1>DICE GAME</h1>
+        <Button onClick={toggle}>Play Now</Button>
+      </div>
+    </Container>
+  )
+}
+
+export default HomePage
+
+const Container = styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .content{
+      display: flex;
+      flex-direction: column;
+      align-items: end;
+
+      h1{     /* --> Selecting class "content" h1 tag inside the "Container" */
+        font-size: 96px;
+        font-weight: 700;
+        white-space: nowrap;
+      }
+    }
+`;
